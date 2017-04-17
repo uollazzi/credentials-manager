8303d5a3835af3a91dd90c2c954afef95623143d
Uollazzi <dilloawallace@hotmail.com>
2017-04-17 16:51:33 +0200

    init db

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const msg = require('./msg');
 
 const dbPath = 'data.json';
 
@@ -6,6 +7,11 @@ let db;
 
 module.exports = {
   load: () => {
+    if (!fs.exists(dbPath)) {
+      fs.writeFileSync(dbPath, JSON.stringify([]));
+      msg.info(dbPath, "created")
+    }
+
     db = JSON.parse(fs.readFileSync(dbPath));
     return db;
   },
